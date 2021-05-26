aa4e561454f386b3da1fb48603fcb7d42eccfbe2
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-26 15:35:45 +0530

    task lwc

---
+++ force-app/main/default/lwc/task/task.js
@@ -0,0 +1,14 @@
+/**
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 05-26-2021
+ * @last modified by  : Vrushabh Uprikar
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   05-26-2021   Vrushabh Uprikar   Initial Version
+**/
+import { api, LightningElement } from 'lwc';
+
+export default class Task extends LightningElement
+{
+    @api title;
+}
