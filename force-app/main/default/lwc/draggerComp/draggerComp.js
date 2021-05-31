79d9032c7ba4d0bb367e61420ea94fa76447ba31
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-31 20:20:21 +0530

    drag comp 1

--- force-app/main/default/lwc/draggerComp/draggerComp.js
+++ force-app/main/default/lwc/draggerComp/draggerComp.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+/**
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 05-31-2021
+ * @last modified by  : Vrushabh Uprikar
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   05-31-2021   Vrushabh Uprikar   Initial Version
+**/
+import { LightningElement, wire } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountHelper.getAccountList';
+export default class DraggerComp extends LightningElement
+{
+    @wire(getAccountList) accList;
+    handleDragStartEvnt(event)
+    {
+        event.dataTransfer.setData("accountId", event.target.dataset.item);
 
-export default class DraggerComp extends LightningElement {}
+    }
+
+}
